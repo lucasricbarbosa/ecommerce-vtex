3c9b3d214245255ce9306e2f85095b3682092a83
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-07-19 12:55:11 -0300

    adding sections

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     slidesPerView: 4,
-      spaceBetween: 62,
-  
+    spaceBetween: 62,
+
     // If we need pagination
 
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+    },
+
     // And if we need scrollbar
+
+});
+
+let swiper2 = new Swiper('#swiper-container2', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+        
+    },
     
-  });
+});
+
