a1d3944f9143b35a1d4e3d4289a370f45bc53b77
Lucas Ricardo <lucasricardobarb@gmail.com>
2023-07-19 00:48:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 4,
+      spaceBetween: 62,
+  
+    // If we need pagination
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    
+  });
